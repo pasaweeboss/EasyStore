c15ef7b6d1b815a5b7b7ba3842ee442c1c9d135d
boss <pasawee.boss@gmail.com>
2018-09-25 15:56:38 +0700

    permisstion internet success

--- app/src/main/java/boss/manin/pasawee/easystore/RegisterFragment.java
+++ app/src/main/java/boss/manin/pasawee/easystore/RegisterFragment.java
@@ -4,11 +4,91 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create toolbar
+        createToolbar();
+
+    } //main method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemUpload) {
+
+//            Get Value from EditText
+            EditText nameEditText = getView().findViewById(R.id.edtName);
+            EditText userEditText = getView().findViewById(R.id.edtUser);
+            EditText passEditText = getView().findViewById(R.id.edtPassword);
+//text change to string
+            String nameString = nameEditText.getText().toString().trim();
+            String userString = userEditText.getText().toString().trim();
+            String passwordString = passEditText.getText().toString().trim();
+
+            // check space in register
+            if (nameString.isEmpty()||userString.isEmpty()||passwordString.isEmpty()) {
+//                Have space
+                MyAlert myAlert = new MyAlert(getActivity());
+                myAlert.normalDialog("Have Space","Please Fill All Blank");
+
+
+            } else {
+//                No space
+            }
+
+
+
+
+
+        } //if item upload
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_register,menu);
+
+    }
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please fill all Blank");
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setHasOptionsMenu(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+
+
+
+    }
 
     @Nullable
     @Override
